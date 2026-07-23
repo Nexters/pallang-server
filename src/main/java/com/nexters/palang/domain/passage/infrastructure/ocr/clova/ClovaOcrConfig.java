c15281ec56ae1ce312841a1288a3f5af300eff07
jinnieusLab <kitty14904@naver.com>
2026-07-23 22:39:12 +0900

    :wrench: chore: 네이버 클로바 OCR 관련 설정 추가

---
+++ src/main/java/com/nexters/palang/domain/passage/infrastructure/ocr/clova/ClovaOcrConfig.java
@@ -0,0 +1,9 @@
+package com.nexters.palang.domain.passage.infrastructure.ocr.clova;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(ClovaOcrProperties.class)
+public class ClovaOcrConfig {
+}
