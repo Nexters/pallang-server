75467dddf87d91b135d335436726fa57a8a322fc
[CLOUD4] 백승주 <bsj5967@naver.com>
2026-07-26 11:01:25 +0900

    ✨ feat: User 닉네임/배경색 변경 및 에러코드 추가
    changeNickname()은 nicknameUpdatedAt이 오늘 날짜면(달력일 기준)
    USER_400_1로 재변경을 막고, changeBackgroundColor()는 단순 필드
    갱신이다. 닉네임 중복은 단일 엔티티만으로 판단할 수 없어 서비스
    계층에서 검증하도록 UserRepository.existsByNicknameAndIdNot을
    추가했다. UserErrorCode에 USER_400_1(변경 제한), USER_409_1(중복)을
    추가한다.

--- src/main/java/com/nexters/palang/domain/user/infrastructure/UserRepository.java
+++ src/main/java/com/nexters/palang/domain/user/infrastructure/UserRepository.java
@@ -4,4 +4,6 @@ import com.nexters.palang.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByNicknameAndIdNot(String nickname, Long id);
 }
